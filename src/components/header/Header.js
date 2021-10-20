66ddb45402b97aa05d23072b025d137d7a7596f5
Cordell <cmaines79@gmail.com>
2021-10-20 15:45:16 -0600

    minor changes

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+// importing stylesheets and assests
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div></div>
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="nav">
+                {/* enter nav here */}
+            </div>
+        </header>
+    )
+}
+
+export default Header
