4c32571d557a39517ad1f0381723294477b820da
Cordell <cmaines79@gmail.com>
2021-10-20 15:03:28 -0600

    Basic Router setup

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+// importing modules
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './App';
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes;
